1a27664fc9cbd4a1fae5245d5147e114595a1e9b
hiltjar000 <hiltjar000@warren.k12in.us>
2017-02-27 23:04:35 -0500

    Bullet class

---
+++ src/Bullet.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+
+/**
+ * Created by Jared H on 2/27/2017.
+ */
+public class Bullet extends Entity{
+
+    public Bullet(Color color, int x, int y, int w, int h, Game game) {
+        super(color, x, y, w, h, game);
+    }
+
+    public void move(){
+
+    }
+
+    public void paint(Graphics g){
+
+    }
+}
